e9f668f64c5ca2e928d27508701355ef8ba7658e
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-08 18:52:37 -0500

    Add index file with routing defined

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once __DIR__ . '/../includes/app.php';
+
+use Controller\LoginController;
+use MVC\Router;
+$router = new Router();
+
+// Log in
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+
+// Password recovery
+$router->get('/forgot-password', [LoginController::class, 'forgotPassword']);
+$router->post('/forgot-password', [LoginController::class, 'forgotPassword']);
+$router->get('/recover-password', [LoginController::class, 'recoverPassword']);
+$router->post('/recover-password', [LoginController::class, 'recoverPassword']);
+
+// Sign up
+$router->get('/sign-up', [LoginController::class, 'signUp']);
+$router->post('/sign-up', [LoginController::class, 'signUp']);
+
+// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
+$router->comprobarRutas();
