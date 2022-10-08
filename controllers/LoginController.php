90d2b68fa3cd143d6b668ad540382b43f252b3d8
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-08 18:42:33 -0500

    Add controllers folder with the login methods (not defined)

---
+++ controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Controller;
+
+class LoginController
+{
+    public static function login()
+    {
+        echo "from login";
+    }
+
+    public static function logout()
+    {
+        echo "from logout";
+    }
+
+    public static function forgotPassword()
+    {
+        echo "from forgot-password";
+    }
+
+    public static function recoverPassword()
+    {
+        echo "from recover-password";
+    }
+
+    public static function signUp()
+    {
+        echo "from sign-up";
+    }
+}
