daf32bcec8b9f5ead9d8f9a0a421e9c4bec50abb
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-13 01:29:54 -0500

    adds client-side fetch and displays items in the view

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,9 @@ function iniciarApp() {
 
   paginaAnterior();
   paginaSiguiente();
+
+  // Consulta la API en el backend de PHP
+  consultarAPI();
 }
 
 function mostrarSeccion() {
@@ -85,5 +88,40 @@ function paginaSiguiente() {
     else paso++;
 
     botonesPaginador();
+  });
+}
+
+async function consultarAPI() {
+  try {
+    const url = "http://localhost:3000/api/services";
+    const resultado = await fetch(url);
+    const servicios = await resultado.json();
+
+    mostrarServicios(servicios);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function mostrarServicios(servicios) {
+  servicios.forEach((servicio) => {
+    const { id, nombre, precio } = servicio;
+
+    const nombreServcio = document.createElement("P");
+    nombreServcio.classList.add("nombre-servicio");
+    nombreServcio.textContent = nombre;
+
+    const precioServicio = document.createElement("P");
+    precioServicio.classList.add("precio-servicio");
+    precioServicio.textContent = `$${precio}`;
+
+    const servicioDiv = document.createElement("DIV");
+    servicioDiv.classList.add("servicio");
+    servicioDiv.dataset.idServicio = id;
+
+    servicioDiv.appendChild(nombreServcio);
+    servicioDiv.appendChild(precioServicio);
+
+    document.querySelector("#servicios").appendChild(servicioDiv);
   });
 }
