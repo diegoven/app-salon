1ff91d68907de7dfa0b777481cda4dd4307d7176
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-14 09:16:24 -0500

    add save functionality to the api

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -2,6 +2,8 @@
 
 namespace Controller;
 
+use Model\Cita;
+use Model\CitaServicio;
 use Model\Servicio;
 
 class APIController
@@ -10,5 +12,28 @@ class APIController
     {
         $servicios = Servicio::all();
         echo json_encode($servicios);
+    }
+
+    public static function save()
+    {
+        // Almacena la cita y devuelve el ID
+        $cita = new Cita($_POST);
+        $resultado = $cita->guardar();
+
+        $idCita = $resultado['id'];
+
+        // Almacena los servicios con el ID de la cita
+        $idServicios = explode(",", $_POST['servicios']);
+
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $idCita,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        echo json_encode(['resultado' => $resultado]);
     }
 }
