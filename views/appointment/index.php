5dbad829be4e7fcdc5e3cbf9eaf29793b3b2a32e
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-14 09:44:06 -0500

    add styles to logout button

--- views/appointment/index.php
+++ views/appointment/index.php
@@ -1,5 +1,6 @@
 <div class="barra">
     <p>Hola, <?php echo $nombre; ?></p>
+    <a href="/logout" class="boton">Cerrar sesión</a>
 </div>
 <h1 class="nombre-pagina">Crear cita nueva</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos:</p>
