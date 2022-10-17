df67df2089a52296125ec44d9fd877b2a8559df7
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-17 17:37:08 -0500

    add menu bar for admin view

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -2,3 +2,11 @@
     <p>Hola, <?php echo $nombre; ?></p>
     <a href="/logout" class="boton">Cerrar sesión</a>
 </div>
+
+<?php if (isset($_SESSION['admin'])) : ?>
+    <div class="barra-servicios">
+        <a href="/admin" class="boton">Ver citas</a>
+        <a href="/services" class="boton">Ver servicios</a>
+        <a href="/services/create" class="boton">Crear servicio</a>
+    </div>
+<?php endif; ?>
